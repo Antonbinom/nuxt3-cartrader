e4d9317683d943ac1066906394e96ea0ce38120e
Antonbinom <protest_the_hero@mail.ru>
2024-01-22 15:50:34 +0530

    add: dynamic card render,  error page, sidebar filter

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <CarDetailsHero />
-    <CarDetailsAttribute />
-    <CarDetailsDescription />
+    <CarDetailsHero :car="car" />
+    <CarDetailsAttribute :features="car.features" />
+    <CarDetailsDescription :description="car.description" />
     <CarDetailsContact />
   </div>
 </template>
@@ -10,11 +10,23 @@
 <script setup>
 const { toTitleCase } = useUtils();
 const route = useRoute();
-definePageMeta({
-  layout: 'custom'
-})
-useHead({
-  title: toTitleCase(route.params.name)
-})
+const { cars } = useCars();
 
+definePageMeta({
+  layout: "custom",
+});
+useHead({
+  title: toTitleCase(route.params.name),
+});
+
+const car = computed(() => {
+  return cars.find((c) => c.id === parseInt(route.params.id));
+});
+
+if (!car.value) {
+  throw createError({
+    statusCode: 404,
+    message: `Car with ID of ${route.params.id} does not exist`,
+  });
+}
 </script>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
     <div class="mt-32 flex">
-      <CarSideBar />
-      <NuxtPage />
+      <NuxtErrorBoundary>
+        <CarSideBar />
+        <NuxtPage />
+        <template #error="{ error }">
+          <div class="text-center mx-auto flex flex-col">
+            <h1 class="text-5xl text-red-600 mb-4">
+              Sorry, something went wrong
+            </h1>
+            <code>{{ error }}</code>
+            <button
+              @click="error.value = null"
+              class="text-center text-white bg-blue-400 px-10 py-3 rounded mt-4"
+            >
+              Go back
+            </button>
+          </div>
+        </template>
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
 
 <script setup>
-const { getPageTitle } = useUtils()
+const { getPageTitle } = useUtils();
 
 definePageMeta({
-  layout: 'custom'
-})
+  layout: "custom",
+});
 
 useHead({
-  title: getPageTitle()
-})
-
+  title: getPageTitle(),
+});
 </script>
